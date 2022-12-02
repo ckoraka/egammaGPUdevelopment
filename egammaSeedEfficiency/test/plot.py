b515be34e9df0ba7946d67f784dcaeedaf9fbf40
charis kleio koraka <charis.kleio.koraka@cern.ch>
2022-12-02 08:27:07 -0600

    Adapt plotting macro for new trees and add doublet combination plots

--- egammaSeedEfficiency/test/plot.py
+++ egammaSeedEfficiency/test/plot.py
@@ -30,6 +30,55 @@ def parse_arguments():
 def createDir(path):
     if not os.path.exists(path):
         os.makedirs(path)
+  
+def doubletComb(BPX1,BPX2,BPX3,BPX4,FPXp1,FPXp2,FPXp3,FPXn1,FPXn2,FPXn3):
+
+    doublets = [0]*20
+
+    if(BPX1>0 and BPX2>0):
+        doublets[0]=1
+    if(BPX1>0 and FPXp1>0):
+        doublets[1]=1
+    if(BPX1>0 and FPXn1>0):
+        doublets[2]=1
+    if(BPX2>0 and BPX3>0):
+        doublets[3]=1
+    if(BPX2>0 and FPXp1>0):
+        doublets[4]=1
+    if(BPX2>0 and FPXn1>0):
+        doublets[5]=1
+    if(FPXp1>0 and FPXp2>0):
+        doublets[6]=1
+    if(FPXn1>0 and FPXn2>0):
+        doublets[7]=1
+    if(BPX3>0 and BPX4>0):
+        doublets[8]=1
+    if(BPX3>0 and FPXp1>0):
+        doublets[9]=1
+    if(BPX3>0 and FPXn1>0):
+        doublets[10]=1
+    if(FPXp2>0 and FPXp3>0):
+        doublets[11]=1
+    if(FPXn2>0 and FPXn3>0):
+        doublets[12]=1
+    if(BPX1>0 and BPX3>0):
+        doublets[13]=1
+    if(BPX2>0 and BPX4>0):
+        doublets[14]=1
+    if(BPX1>0 and FPXp2>0):
+        doublets[15]=1
+    if(BPX1>0 and FPXn2>0):
+        doublets[16]=1
+    if(FPXp1>0 and FPXp3>0):
+        doublets[17]=1
+    if(FPXn1>0 and FPXn3>0):
+        doublets[18]=1
+
+    # Check if there are any other type of combinations
+    if(sum(doublets)==0 and ((min(BPX1,1)+min(BPX2,1)+min(BPX3,1)+min(BPX4,1)+min(FPXp1,1)+min(FPXp2,1)+min(FPXp3,1)+min(FPXn1,1)+min(FPXn2,1)+min(FPXn3,1))>1)):  
+        doublets[19]=1
+
+    return doublets
 
 def main(options, paths):
 
@@ -43,7 +92,6 @@ def main(options, paths):
     nSimHitsL1FPIX = r.TH1F("nSimHitsL1FPIX",";Number of FPIX Hits in Layer1 ",8,0,8) 
     nSimHitsL2FPIX = r.TH1F("nSimHitsL2FPIX",";Number of FPIX Hits in Layer2",8,0,8)  
     nSimHitsL3FPIX = r.TH1F("nSimHitsL3FPIX",";Number of FPIX Hits in Layer3",8,0,8)  
-    nSimHitsL4FPIX = r.TH1F("nSimHitsL4FPIX",";Number of FPIX Hits in Layer4",8,0,8)
     nSimHitsTotal= r.TH1F("nSimHitsTotal",";Number of Total Layers with Hits",9,0,9) 
 
     simElepT = r.TH1F("simPt",";p_{T} [GeV]",20,0.,100.) 
@@ -61,7 +109,6 @@ def main(options, paths):
     nRecoHitsL1FPIX = r.TH1F("nRecoHitsL1FPIX",";Number of FPIX Hits in Layer1 ",8,0,8)
     nRecoHitsL2FPIX = r.TH1F("nRecoHitsL2FPIX",";Number of FPIX Hits in Layer2",8,0,8)
     nRecoHitsL3FPIX = r.TH1F("nRecoHitsL3FPIX",";Number of FPIX Hits in Layer3",8,0,8) 
-    nRecoHitsL4FPIX = r.TH1F("nRecoHitsL4FPIX",";Number of FPIX Hits in Layer4",8,0,8) 
 
     nRecoHitsTotal= r.TH1F("nRecoHitsTotal",";Number of Total Layers with Hits",9,0,9) 
 
@@ -70,6 +117,14 @@ def main(options, paths):
     nSimvsRecoDiffBPIX= r.TH1F("nSimvsRecoDiffBPIX",";Number of BPIX Layers with Sim Hits - Number of BPIX Layers with Reco Hits",12,-6,6) 
     nSimvsRecoDiffFPIX= r.TH1F("nSimvsRecoDiffFPIX",";Number of FPIX Layers with Sim Hits - Number of FPIX Layers with Reco Hits",12,-6,6) 
 
+    nSimVsRecoHitsTotal = r.TH2F("recovssimTotal",";Total number of Layers with Sim Hits;Total number of Layers with Reco Hits",8,0.,8.,8,0.,8.) 
+    nSimvsRecoDiffTotal= r.TH1F("nSimvsRecoDiffTotal",";Total number of Layers with Sim Hits - Total number of Layers with Reco Hits",16,-8,8) 
+
+    numberOfPixelDoubletsSim = r.TH1F("numberOfPixelDoubletsSim",";Number of Pixel doublets per TP",10,0,10) 
+    numberOfPixelDoubletsReco = r.TH1F("numberOfPixelDoubletsReco",";Number of Pixel doublets per TP",10,0,10) 
+
+    pixelDoubletTypeSim = r.TH1F("pixelDoubletTypeSim",";Type of Pixel doublet ",20,0,20) 
+    pixelDoubletTypeReco  = r.TH1F("pixelDoubletTypeReco",";Type of Pixel doublet ",20,0,20) 
 
     recoElepT = r.TH1F("recoPt",";p_{T} [GeV]",20,0.,100.) 
     recoEleEta = r.TH1F("recoEta",";#eta",20,-3.,3.) 
@@ -79,7 +134,7 @@ def main(options, paths):
 
 
     f = r.TFile.Open(options.input)
-    for event in f.Get("egammaReconstruction/tree"):
+    for event in f.Get("egammaReconstructionEB/tree"):
         for electron in range(0,len(event.nSimHitLayersBPIX)):
             #if(event.isMatched[electron]):
             nSimHits.Fill(event.nSimHitLayersBPIX[electron])  
@@ -88,10 +143,10 @@ def main(options, paths):
             nSimHitsL3.Fill(event.nSimHitsLayer3_BPIX[electron])  
             nSimHitsL4.Fill(event.nSimHitsLayer4_BPIX[electron])  
             nSimHitsFPIX.Fill(event.nSimHitLayersFPIX[electron])  
-            nSimHitsL1FPIX.Fill(event.nSimHitsLayer1_FPIX[electron])  
-            nSimHitsL2FPIX.Fill(event.nSimHitsLayer2_FPIX[electron])  
-            nSimHitsL3FPIX.Fill(event.nSimHitsLayer3_FPIX[electron])  
-            nSimHitsL4FPIX.Fill(event.nSimHitsLayer4_FPIX[electron])  
+            nSimHitsL1FPIX.Fill(event.nSimHitsLayer1_FPIX_pos[electron] + event.nSimHitsLayer1_FPIX_neg[electron])  
+            nSimHitsL2FPIX.Fill(event.nSimHitsLayer2_FPIX_pos[electron] + event.nSimHitsLayer2_FPIX_neg[electron])  
+            nSimHitsL3FPIX.Fill(event.nSimHitsLayer3_FPIX_pos[electron] + event.nSimHitsLayer3_FPIX_neg[electron])  
+
             simElepT.Fill(event.simEle_pt[electron]) 
             simEleEta.Fill(event.simEle_eta[electron]) 
             simElePhi.Fill(event.simEle_phi[electron]) 
@@ -105,15 +160,29 @@ def main(options, paths):
                 totalLayers = totalLayers+1
             if(event.nSimHitsLayer4_BPIX[electron]>0):
                 totalLayers = totalLayers+1
-            if(event.nSimHitsLayer1_FPIX[electron]>0):
+            if(event.nSimHitsLayer1_FPIX_pos[electron]>0):
                 totalLayers = totalLayers+1
-            if(event.nSimHitsLayer2_FPIX[electron]>0):
+            if(event.nSimHitsLayer1_FPIX_neg[electron]>0):
+                totalLayers = totalLayers+1    
+            if(event.nSimHitsLayer2_FPIX_pos[electron]>0):
+                totalLayers = totalLayers+1    
+            if(event.nSimHitsLayer2_FPIX_neg[electron]>0):
                 totalLayers = totalLayers+1
-            if(event.nSimHitsLayer3_FPIX[electron]>0):
+            if(event.nSimHitsLayer3_FPIX_pos[electron]>0):
                 totalLayers = totalLayers+1
-            if(event.nSimHitsLayer4_FPIX[electron]>0):
+            if(event.nSimHitsLayer3_FPIX_neg[electron]>0):
                 totalLayers = totalLayers+1
             nSimHitsTotal.Fill(totalLayers)
+
+            doubletsSim = doubletComb(event.nSimHitsLayer1_BPIX[electron],event.nSimHitsLayer2_BPIX[electron],event.nSimHitsLayer3_BPIX[electron],event.nSimHitsLayer4_BPIX[electron],
+                                   event.nSimHitsLayer1_FPIX_pos[electron],event.nSimHitsLayer2_FPIX_pos[electron],event.nSimHitsLayer3_FPIX_pos[electron],
+                                   event.nSimHitsLayer1_FPIX_neg[electron],event.nSimHitsLayer2_FPIX_neg[electron],event.nSimHitsLayer3_FPIX_neg[electron])
+        
+            for index in range(0,len(doubletsSim)):
+                pixelDoubletTypeSim.Fill(index,doubletsSim[index])
+ 
+            numberOfPixelDoubletsSim.Fill(sum(doubletsSim))
+
 
         if(len(event.simEle_pt)==2):
             v1 = r.TLorentzVector()
@@ -129,10 +198,10 @@ def main(options, paths):
             nRecoHitsL3.Fill(event.nRecoHitsLayer3_BPIX[electron])  
             nRecoHitsL4.Fill(event.nRecoHitsLayer4_BPIX[electron])  
             nRecoHitsFPIX.Fill(event.nRecoHitLayersFPIX[electron])  
-            nRecoHitsL1FPIX.Fill(event.nRecoHitsLayer1_FPIX[electron])  
-            nRecoHitsL2FPIX.Fill(event.nRecoHitsLayer2_FPIX[electron])  
-            nRecoHitsL3FPIX.Fill(event.nRecoHitsLayer3_FPIX[electron])  
-            nRecoHitsL4FPIX.Fill(event.nRecoHitsLayer4_FPIX[electron])  
+            nRecoHitsL1FPIX.Fill(event.nRecoHitsLayer1_FPIX_pos[electron] + event.nRecoHitsLayer1_FPIX_neg[electron])  
+            nRecoHitsL2FPIX.Fill(event.nRecoHitsLayer2_FPIX_pos[electron] + event.nRecoHitsLayer2_FPIX_neg[electron])  
+            nRecoHitsL3FPIX.Fill(event.nRecoHitsLayer3_FPIX_pos[electron] + event.nRecoHitsLayer3_FPIX_neg[electron])  
+
             index=0
             if(event.isMatched[electron]):
                 recoElepT.Fill(event.recoEle_pt[index]) 
@@ -145,25 +214,42 @@ def main(options, paths):
                 nSimvsRecoDiffBPIX.Fill(event.nSimHitLayersBPIX[electron]-event.nRecoHitLayersBPIX[electron])
                 nSimvsRecoDiffFPIX.Fill(event.nSimHitLayersFPIX[electron]-event.nRecoHitLayersFPIX[electron])
 
-            totalLayers = 0
+            totalLayersReco = 0
             if(event.nRecoHitsLayer1_BPIX[electron]>0):
-                totalLayers = totalLayers+1
+                totalLayersReco = totalLayersReco+1
             if(event.nRecoHitsLayer2_BPIX[electron]>0):
-                totalLayers = totalLayers+1
+                totalLayersReco = totalLayersReco+1
             if(event.nRecoHitsLayer3_BPIX[electron]>0):
-                totalLayers = totalLayers+1
+                totalLayersReco = totalLayersReco+1
             if(event.nRecoHitsLayer4_BPIX[electron]>0):
-                totalLayers = totalLayers+1
-            if(event.nRecoHitsLayer1_FPIX[electron]>0):
-                totalLayers = totalLayers+1
-            if(event.nRecoHitsLayer2_FPIX[electron]>0):
-                totalLayers = totalLayers+1
-            if(event.nRecoHitsLayer3_FPIX[electron]>0):
-                totalLayers = totalLayers+1
-            if(event.nRecoHitsLayer4_FPIX[electron]>0):
-                totalLayers = totalLayers+1
-            nRecoHitsTotal.Fill(totalLayers)
+                totalLayersReco = totalLayersReco+1
+            if(event.nRecoHitsLayer1_FPIX_pos[electron]>0):
+                totalLayersReco = totalLayersReco+1
+            if(event.nRecoHitsLayer1_FPIX_neg[electron]>0):
+                totalLayersReco = totalLayersReco+1                
+            if(event.nRecoHitsLayer2_FPIX_pos[electron]>0):
+                totalLayersReco = totalLayersReco+1
+            if(event.nRecoHitsLayer2_FPIX_neg[electron]>0):
+                totalLayersReco = totalLayersReco+1                
+            if(event.nRecoHitsLayer3_FPIX_pos[electron]>0):
+                totalLayersReco = totalLayersReco+1
+            if(event.nRecoHitsLayer3_FPIX_neg[electron]>0):
+                totalLayersReco = totalLayersReco+1
 
+            nRecoHitsTotal.Fill(totalLayersReco)
+
+            doubletsReco = doubletComb(event.nRecoHitsLayer1_BPIX[electron],event.nRecoHitsLayer2_BPIX[electron],event.nRecoHitsLayer3_BPIX[electron],event.nRecoHitsLayer4_BPIX[electron],
+                                   event.nRecoHitsLayer1_FPIX_pos[electron],event.nRecoHitsLayer2_FPIX_pos[electron],event.nRecoHitsLayer3_FPIX_pos[electron],
+                                   event.nRecoHitsLayer1_FPIX_neg[electron],event.nRecoHitsLayer2_FPIX_neg[electron],event.nRecoHitsLayer3_FPIX_neg[electron])
+
+            for index in range(0,len(doubletsReco)):
+                pixelDoubletTypeReco.Fill(index,doubletsReco[index])
+
+            numberOfPixelDoubletsReco.Fill(sum(doubletsReco))
+
+            if(len(event.nSimHitLayersBPIX)==len(event.nRecoHitLayersBPIX)):
+                nSimVsRecoHitsTotal.Fill(totalLayers,totalLayersReco)
+                nSimvsRecoDiffTotal.Fill(totalLayers-totalLayersReco)
 
         if(len(event.recoEle_pt)==2):
             v1 = r.TLorentzVector()
@@ -194,7 +280,6 @@ def main(options, paths):
     l0.Draw("same")
     c.SaveAs('nHitsBPIX.png')
 
-
     c = r.TCanvas("c", "canvas", 900, 700)
     c.cd()
     nSimHitsTotal.SetLineColor(r.kRed)
@@ -206,7 +291,6 @@ def main(options, paths):
     paveCMS.Draw("same")
     l0.Draw("same")
     c.SaveAs('nHitsTotal.png')
-
 
     c = r.TCanvas("c", "canvas", 900, 700)
     c.cd()
@@ -309,18 +393,6 @@ def main(options, paths):
 
     c = r.TCanvas("c", "canvas", 900, 700)
     c.cd()
-    nSimHitsL4FPIX.SetLineColor(r.kRed)
-    nSimHitsL4FPIX.SetLineWidth(2)
-    nRecoHitsL4FPIX.SetLineColor(r.kBlue)
-    nRecoHitsL4FPIX.SetLineWidth(2)
-    nRecoHitsL4FPIX.Draw("HIST")
-    nSimHitsL4FPIX.Draw("HIST same")
-    paveCMS.Draw("same")
-    l0.Draw("same")
-    c.SaveAs('nHitsL4FPIX.png')  
-
-    c = r.TCanvas("c", "canvas", 900, 700)
-    c.cd()
     simElepT.SetLineColor(r.kRed)
     simElepT.SetLineWidth(2)
     recoElepT.SetLineColor(r.kBlue)
@@ -383,6 +455,12 @@ def main(options, paths):
 
     c = r.TCanvas("c", "canvas", 900, 700)
     c.cd()
+    nSimVsRecoHitsTotal.Draw("COLZ TEXT")
+    paveCMS.Draw("same")
+    c.SaveAs('SimVsRecoTotal.png')  
+
+    c = r.TCanvas("c", "canvas", 900, 700)
+    c.cd()
     nSimvsRecoDiffBPIX.Draw("HIST")
     paveCMS.Draw("same")
     c.SaveAs('SimDiffRecoBPIX.png')  
@@ -392,6 +470,36 @@ def main(options, paths):
     nSimvsRecoDiffFPIX.Draw("HIST")
     paveCMS.Draw("same")
     c.SaveAs('SimDiffRecoFPIX.png')  
+
+    c = r.TCanvas("c", "canvas", 900, 700)
+    c.cd()
+    nSimvsRecoDiffTotal.Draw("HIST")
+    paveCMS.Draw("same")
+    c.SaveAs('SimDiffRecoTotal.png')  
+
+    c = r.TCanvas("c", "canvas", 900, 700)
+    c.cd()
+    pixelDoubletTypeSim.SetLineColor(r.kRed)
+    pixelDoubletTypeSim.SetLineWidth(2)
+    pixelDoubletTypeReco.SetLineColor(r.kBlue)
+    pixelDoubletTypeReco.SetLineWidth(2)
+    pixelDoubletTypeSim.Draw("HIST")
+    pixelDoubletTypeReco.Draw("HIST same")
+    paveCMS.Draw("same")
+    l0.Draw("same")
+    c.SaveAs('pixelDoubletTypes.png')  
+
+    c = r.TCanvas("c", "canvas", 900, 700)
+    c.cd()
+    numberOfPixelDoubletsSim.SetLineColor(r.kRed)
+    numberOfPixelDoubletsSim.SetLineWidth(2)
+    numberOfPixelDoubletsReco.SetLineColor(r.kBlue)
+    numberOfPixelDoubletsReco.SetLineWidth(2)
+    numberOfPixelDoubletsSim.Draw("HIST")
+    numberOfPixelDoubletsReco.Draw("HITS same")
+    paveCMS.Draw("same")
+    l0.Draw("same")
+    c.SaveAs('NumberOfDoublets.png')  
 
 
 if __name__ == '__main__':
